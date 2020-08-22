e6f251c5a6ef4c11dfa8fe92ec8df5f275f168f4
gragerdsouza <gragerdsouza@gmail.com>
2020-08-22 12:41:56 +0100

    changes in routes

--- routes/api.php
+++ routes/api.php
@@ -15,9 +15,23 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
-    //return $request->user();
+	//$user = Auth::user();
+//return $user->only(['id', 'name', 'email']);
 	return response()->json([
-		'response' => $request->user(),
-		'status' => 200
+			'response' => $request->user()->only(['id', 'name', 'email']),
+			'status' => true
 	]) ->header('Content-Type', 'application/json');
 });
+
+//Route::post('register', 'Auth\RegisterController@register');
+Route::post('login', 'AuthController@login');
+//Route::post('refresh', 'Auth\LoginController@refresh');
+//Route::post('login', 'LoginController@login');
+/*Route::group(['middleware' => 'auth:api'], function() {
+Route::post('logout', 'Auth\LoginController@logout');
+	Route::get('articles', 'ArticleController@index');
+	Route::get('articles/{article}', 'ArticleController@show');
+	Route::post('articles', 'ArticleController@store');
+	Route::put('articles/{article}', 'ArticleController@update');
+	Route::delete('articles/{article}', 'ArticleController@delete');
+});*/
